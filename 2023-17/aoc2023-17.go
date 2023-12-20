afbdab11ac0a956e7ac9ddc9cb77046519666ba4
Tarmo Tali <tarmo@tali.land>
2023-12-20 22:23:44 +0200

    AOC 2023 17 - Nope, Dijkstra alone does not help

--- 2023-17/aoc2023-17.go
+++ 2023-17/aoc2023-17.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 type Direction int
@@ -13,84 +14,187 @@ const (
 	Right
 )
 
-func getFirstHeatLoss(data [][]int) int {
-	minHeatLoss := 0
-	for x := 0; x < len(data)-1; x++ {
-		minHeatLoss += data[x+1][x]
-		minHeatLoss += data[x+1][x+1]
-	}
-	return minHeatLoss
+type Point struct {
+	x, y int
 }
 
-func findPath(data [][]int, x, y int, direction Direction, straightSteps int, sum int, minHeatLoss *int, path *Stack) {
-	if x < 0 || y < 0 || x >= len(data) || y >= len(data[0]) {
-		return
-	}
-	if data[x][y] < 0 {
-		return
-	}
-	sum += data[x][y]
-	if sum > *minHeatLoss {
-		return
-	}
-	if x == len(data)-1 && y == len(data[0])-1 {
-		fmt.Println("Found path: ", sum)
-		// printPath(path)
-		if sum < *minHeatLoss {
-			*minHeatLoss = sum
+type DirectionCounter struct {
+	direction   Direction
+	count       int
+	minHeatLoss int
+}
+
+type CityBlock struct {
+	value            int
+	visited          bool
+	minHeatLoss      int
+	minHeatLossPoint Point
+}
+
+func createSolvingMap(data [][]int) [][]CityBlock {
+	solvingMap := make([][]CityBlock, len(data))
+	for x := 0; x < len(data); x++ {
+		solvingMap[x] = make([]CityBlock, len(data[0]))
+		for y := 0; y < len(data[0]); y++ {
+			solvingMap[x][y] = CityBlock{data[x][y], false, math.MaxInt, Point{-1, -1}}
 		}
-		return
 	}
-	data[x][y] -= 100
-	// path.Push(Point{x, y, straightSteps})
-	switch direction {
-	case Up:
-		if straightSteps < 3 {
-			findPath(data, x-1, y, Up, straightSteps+1, sum, minHeatLoss, path)
+	return solvingMap
+}
+
+func getSmallestBlock(queue map[Point]DirectionCounter) (Point, DirectionCounter) {
+	min := math.MaxInt
+	maxX := 0
+	maxY := 0
+	var minPoint Point
+	var minDirection DirectionCounter
+	for point, dc := range queue {
+		if dc.minHeatLoss < min {
+			min = dc.minHeatLoss
+			maxX = point.x
+			maxY = point.y
+			minPoint = point
+			minDirection = dc
+		} else if dc.minHeatLoss == min {
+			if point.y > maxY {
+				min = dc.minHeatLoss
+				maxX = point.x
+				maxY = point.y
+				minPoint = point
+				minDirection = dc
+			} else if point.y == maxY {
+				if point.x > maxX {
+					min = dc.minHeatLoss
+					maxY = point.y
+					maxX = point.x
+					minPoint = point
+					minDirection = dc
+				}
+			}
 		}
-		findPath(data, x, y-1, Left, 1, sum, minHeatLoss, path)
-		findPath(data, x, y+1, Right, 1, sum, minHeatLoss, path)
-	case Down:
-		if straightSteps < 3 {
-			findPath(data, x+1, y, Down, straightSteps+1, sum, minHeatLoss, path)
-		}
-		findPath(data, x, y-1, Left, 1, sum, minHeatLoss, path)
-		findPath(data, x, y+1, Right, 1, sum, minHeatLoss, path)
-	case Left:
-		if straightSteps < 3 {
-			findPath(data, x, y-1, Left, straightSteps+1, sum, minHeatLoss, path)
-		}
-		findPath(data, x-1, y, Up, 1, sum, minHeatLoss, path)
-		findPath(data, x+1, y, Down, 1, sum, minHeatLoss, path)
-	case Right:
-		if straightSteps < 3 {
-			findPath(data, x, y+1, Right, straightSteps+1, sum, minHeatLoss, path)
-		}
-		findPath(data, x-1, y, Up, 1, sum, minHeatLoss, path)
-		findPath(data, x+1, y, Down, 1, sum, minHeatLoss, path)
 	}
-	data[x][y] += 100
-	// path.Pop()
+	delete(queue, minPoint)
+	return minPoint, minDirection
+}
+
+func processBlock(currentPoint Point, nextX int, nextY int, currentPointDirection DirectionCounter, dir Direction, solvingMap [][]CityBlock, queue *map[Point]DirectionCounter) {
+	stepCounter := 1
+	if currentPointDirection.direction == dir {
+		stepCounter = currentPointDirection.count + 1
+	}
+	stepCounter = 1
+	nextBlock := solvingMap[nextX][nextY]
+	newMinHeatLoss := nextBlock.value + currentPointDirection.minHeatLoss
+	if nextBlock.minHeatLoss > newMinHeatLoss {
+		solvingMap[nextX][nextY].minHeatLoss = newMinHeatLoss
+		solvingMap[nextX][nextY].minHeatLossPoint = Point{currentPoint.x, currentPoint.y}
+		(*queue)[Point{nextX, nextY}] = DirectionCounter{dir, stepCounter, newMinHeatLoss}
+	}
+}
+
+func resolveUp(currentPoint Point, currentPointDirection DirectionCounter, solvingMap [][]CityBlock, queue *map[Point]DirectionCounter) {
+	nextX := currentPoint.x - 1
+	nextY := currentPoint.y
+	if nextX >= 0 && !solvingMap[nextX][nextY].visited {
+		// Moving Up is valid move
+		processBlock(currentPoint, nextX, nextY, currentPointDirection, Up, solvingMap, queue)
+	}
+}
+
+func resolveRight(currentPoint Point, currentPointDirection DirectionCounter, solvingMap [][]CityBlock, queue *map[Point]DirectionCounter) {
+	nextX := currentPoint.x
+	nextY := currentPoint.y + 1
+
+	if nextY < len(solvingMap[0]) && !solvingMap[nextX][nextY].visited {
+		// Moving Right is valid move
+		processBlock(currentPoint, nextX, nextY, currentPointDirection, Right, solvingMap, queue)
+	}
+}
+
+func resolveDown(currentPoint Point, currentPointDirection DirectionCounter, solvingMap [][]CityBlock, queue *map[Point]DirectionCounter) {
+	nextX := currentPoint.x + 1
+	nextY := currentPoint.y
+	if nextX < len(solvingMap) && !solvingMap[nextX][nextY].visited {
+		// Moving Down is valid move
+		processBlock(currentPoint, nextX, nextY, currentPointDirection, Down, solvingMap, queue)
+	}
+}
+
+func resolveLeft(currentPoint Point, currentPointDirection DirectionCounter, solvingMap [][]CityBlock, queue *map[Point]DirectionCounter) {
+	nextX := currentPoint.x
+	nextY := currentPoint.y - 1
+	if nextY >= 0 && !solvingMap[nextX][nextY].visited {
+		// Moving Left is valid move
+		processBlock(currentPoint, nextX, nextY, currentPointDirection, Left, solvingMap, queue)
+	}
+}
+
+func calculateDijkstra(solvingMap [][]CityBlock) {
+	queue := make(map[Point]DirectionCounter)
+
+	solvingMap[0][0].value = 0
+	solvingMap[0][0].minHeatLoss = 0
+	queue[Point{0, 0}] = DirectionCounter{Right, 0, 0}
+
+	for len(queue) > 0 {
+		nba, nbd := getSmallestBlock(queue)
+		solvingMap[nba.x][nba.y].visited = true
+		/*
+			if nba.x == len(solvingMap)-1 && nba.y == len(solvingMap[0])-1 {
+				break
+			}
+		*/
+		switch nbd.direction {
+		case Up:
+			resolveRight(nba, nbd, solvingMap, &queue)
+			if nbd.count < 3 {
+				resolveUp(nba, nbd, solvingMap, &queue)
+			}
+			resolveLeft(nba, nbd, solvingMap, &queue)
+
+		case Down:
+			resolveRight(nba, nbd, solvingMap, &queue)
+			if nbd.count < 3 {
+				resolveDown(nba, nbd, solvingMap, &queue)
+			}
+			resolveLeft(nba, nbd, solvingMap, &queue)
+		case Left:
+			resolveDown(nba, nbd, solvingMap, &queue)
+			resolveUp(nba, nbd, solvingMap, &queue)
+			if nbd.count < 3 {
+				resolveLeft(nba, nbd, solvingMap, &queue)
+			}
+		case Right:
+			if nbd.count < 3 {
+				resolveRight(nba, nbd, solvingMap, &queue)
+			}
+			resolveDown(nba, nbd, solvingMap, &queue)
+			resolveUp(nba, nbd, solvingMap, &queue)
+		}
+	}
+}
+func tracePath(solvingMap [][]CityBlock) []Point {
+	path := make([]Point, 0)
+	x := len(solvingMap) - 1
+	y := len(solvingMap[0]) - 1
+	path = append(path, Point{x, y})
+	for x != 0 || y != 0 {
+		p := solvingMap[x][y].minHeatLossPoint
+		x = p.x
+		y = p.y
+		path = append(path, Point{x, y})
+	}
+	return path
 }
 
 func puzzle1(data [][]int) int {
-	min := getFirstHeatLoss(data)
-	path := Stack{}
-	// min = 104
-	fmt.Println("First proxy: ", min)
-	// get first proxy number
-	minDown := min
-	findPath(data, 1, 0, Down, 1, 0, &minDown, &path)
-	minRight := minDown
-	// minRight := min
-	// findPath(data, 0, 1, Right, 1, 0, &minRight, &path)
-
-	if minDown < minRight {
-		min = minDown
-	} else {
-		min = minRight
-	}
-	// printData(data)
+	min := 0
+	solvingMap := createSolvingMap(data)
+	calculateDijkstra(solvingMap)
+	printData(data)
+	printMap(solvingMap)
+	path := tracePath(solvingMap)
+	printMapPath(solvingMap, path)
 	return min
 }
 
@@ -101,8 +205,8 @@ func puzzle2(data [][]int) int {
 
 func main() {
 	// cityMap, _ := loadData("input-test2.txt")
-	// cityMap, _ := loadData("input-test.txt")
-	cityMap, _ := loadData("input.txt")
+	cityMap, _ := loadData("input-test.txt")
+	// cityMap, _ := loadData("input.txt")
 	fmt.Println("Puzzle 1: ", puzzle1(cityMap))
 	fmt.Println("Puzzle 2: ", puzzle2(cityMap))
 }
--- 2023-17/print.go
+++ 2023-17/print.go
@@ -14,9 +14,38 @@ func printData(data [][]int) {
 	fmt.Println()
 }
 
-func printPath(path *Stack) {
-	for i := len(*path) - 1; i >= 0; i-- {
-		fmt.Print((*path)[i])
+func printMap(data [][]CityBlock) {
+	for _, z := range data {
+		for _, y := range z {
+			fmt.Printf("%1d (%3d) %2d,%2d  | ", y.value, y.minHeatLoss, y.minHeatLossPoint.x, y.minHeatLossPoint.y)
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func pathContains(path []Point, point Point) bool {
+	for _, p := range path {
+		if p.x == point.x && p.y == point.y {
+			return true
+		}
+	}
+	return false
+}
+
+func printMapPath(data [][]CityBlock, path []Point) {
+	fmt.Println("Path:", path)
+	for x := 0; x < len(data); x++ {
+		for y := 0; y < len(data[0]); y++ {
+			b := data[x][y]
+			if pathContains(path, Point{x, y}) {
+				fmt.Printf("%1d (%3d) - %5c | ", b.value, b.minHeatLoss, 'o')
+			} else {
+
+				fmt.Printf("%1d (%3d) - %2d,%2d | ", b.value, b.minHeatLoss, b.minHeatLossPoint.x, b.minHeatLossPoint.y)
+			}
+		}
+		fmt.Println()
 	}
 	fmt.Println()
 }
