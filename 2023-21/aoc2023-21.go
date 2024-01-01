2ac75bcef38b74ccb701dd2f1e08080d18bd18e8
Tarmo Tali <tarmo@nordictiger.net>
2024-01-01 18:47:08 +0000

    AOC 2023 21 - What Did It Cost? Everything.

--- 2023-21/aoc2023-21.go
+++ 2023-21/aoc2023-21.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func copyGarden(garden [][]byte) [][]byte {
 	newGarden := make([][]byte, len(garden))
@@ -51,7 +49,7 @@ func updateSteps(steps [][]byte, x, y int, garden [][]byte) {
 	}
 }
 
-func countPlots(garden [][]byte) int {
+func countVisitedPlots(garden [][]byte) int {
 	count := 0
 	for i := 0; i < len(garden); i++ {
 		for j := 0; j < len(garden[i]); j++ {
@@ -63,25 +61,75 @@ func countPlots(garden [][]byte) int {
 	return count
 }
 
-func puzzle1(data [][]byte, reminingSteps int) int {
-	startX := -1
-	startY := -1
-	startX, startY = getStart(data)
-	fmt.Println("Start: ", startX, startY)
-	steps := copyGarden(data)
-	printGarden(steps)
-	data[startX][startY] = '.'
+func runSteps(garden [][]byte, reminingSteps, startX, startY int) int {
+	steps := copyGarden(garden)
+	// printGarden(steps)
+	steps[startX][startY] = 'S'
 	for i := 0; i < reminingSteps; i++ {
-		steps = step(steps, data)
+		steps = step(steps, garden)
 	}
-	fmt.Println()
-	printGarden(steps)
-	sum := countPlots(steps)
+	// fmt.Println()
+	// printGarden(steps)
+	sum := countVisitedPlots(steps)
 	return sum
 }
 
-func puzzle2(data [][]byte) int {
-	sum := 0
+func puzzle1(data [][]byte, reminingSteps int) int {
+	startX, startY := getStart(data)
+	data[startX][startY] = '.'
+	sum := runSteps(data, reminingSteps, startX, startY)
+	return sum
+}
+func puzzle2(data [][]byte, reminingSteps int) int {
+	// Prepare garden for getting reachable plots
+	garden := copyGarden(data)
+	startX, startY := getStart(garden)
+	garden[startX][startY] = '.'
+
+	fullMapPlotsOdd := runSteps(garden, 131+131+65, startX, startY)
+	fullMapPlotsEven := runSteps(garden, 131+131+65+1, startX, startY)
+
+	// there are two stable counts flip-floping 7344, 7451
+	// center is 7344 - let's call it odd, hence 7451 is even
+	sum := fullMapPlotsOdd // central point map
+
+	// how many maps we need to add for moving straight up, right, down, left
+	mapsAdded := reminingSteps / 131
+	// last map is "arrowhead", so not complete
+	fullMaps := mapsAdded - 1
+	evenMaps := fullMaps/2 + 1
+	oddMaps := fullMaps / 2
+	triangle := evenMaps*evenMaps*fullMapPlotsEven + oddMaps*(oddMaps+1)*fullMapPlotsOdd
+	sum += 4 * triangle
+
+	// get arrowheads
+	arrowUp := runSteps(garden, 130, 130, 65)
+	arrowRight := runSteps(garden, 130, 65, 0)
+	arrowDown := runSteps(garden, 130, 0, 65)
+	arrowLeft := runSteps(garden, 130, 65, 130)
+
+	// get corners
+	upperLeftFull := runSteps(garden, 195, 0, 0)
+	upperLeft := runSteps(garden, 64, 0, 0)
+	upperRightFull := runSteps(garden, 195, 0, 130)
+	upperRight := runSteps(garden, 64, 0, 130)
+	lowerRight := runSteps(garden, 64, 130, 130)
+	lowerRightFull := runSteps(garden, 195, 130, 130)
+	lowerLeft := runSteps(garden, 64, 130, 0)
+	lowerLeftFull := runSteps(garden, 195, 130, 0)
+
+	// add arrowheads
+	sum += arrowUp
+	sum += arrowRight
+	sum += arrowDown
+	sum += arrowLeft
+
+	// add diagonals
+	sum += fullMaps*lowerLeftFull + mapsAdded*lowerLeft
+	sum += fullMaps*upperLeftFull + mapsAdded*upperLeft
+	sum += fullMaps*upperRightFull + mapsAdded*upperRight
+	sum += fullMaps*lowerRightFull + mapsAdded*lowerRight
+
 	return sum
 }
 
@@ -90,6 +138,6 @@ func main() {
 	// fmt.Println("Puzzle 1: ", puzzle1(garden, 6))
 	garden := loadData("input.txt")
 	fmt.Println("Puzzle 1: ", puzzle1(garden, 64))
-	// 3660
-	fmt.Println("Puzzle 2: ", puzzle2(garden))
+	garden = loadData("input.txt")
+	fmt.Println("Puzzle 2: ", puzzle2(garden, 26501365))
 }
